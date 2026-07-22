f6efd44e7a4a46a61661bf1dfc0123a38aa0692d
unknown <ahmedmobykhan@gmail.com>
2026-07-22 14:33:00 +0400

    add live position proxy route

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nn1kc65sjm4"
+    "revision": "0.81ap3g99e6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
